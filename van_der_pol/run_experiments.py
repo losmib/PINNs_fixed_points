e8158648b969f0a934678429fa4715e17f6c8e1d
losmib <milos.babic@gmx.at>
2025-04-08 10:22:37 +0200

    new van der pol experiments

--- van_der_pol/run_experiments.py
+++ van_der_pol/run_experiments.py
@@ -24,8 +24,8 @@ def grid_parameters(parameters: Dict[str, Iterable[Any]]) -> Iterable[Dict[str, 
 config_base = load_config('configs/default.yaml')
 param_grid = {
     "mu": [1],
-    "T": [5.0, 7.5, 10.0, 12.5, 15.0],
-    "x0": [0.001, 0.01, 0.1, 0.5, 1.0],
+    "T": [10.0, 12.5, 15.0],
+    "x0": [0.01, 0.1, 1.0],
     "x_t0": [0],
     "network_architectures": [
         (4, 50),
@@ -43,7 +43,7 @@ param_grid = {
         25000,
     ],
     "regularization": [
-        #"no_reg"
+        "no_reg"
         #"reg_derivative",
         "reg_derivative_unstable_fp"
     ],
@@ -119,5 +119,5 @@ for params in grid_parameters(param_grid):
     table_entry["mean_loss"] = np.mean(losses)
     table_entry["loss_successes_percent"] = np.sum(loss_successes) / float(NUM_TRAINING_RUNS)
     results_list.append(table_entry)
-    pd.concat(results_list).to_csv("results_reg_full_newest.csv")
+    pd.concat(results_list).to_csv("results_reg_full_paper.csv")
 
