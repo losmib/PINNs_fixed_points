82762d0a365c379c5a927c2adcee51439bad1593
losmib <milos.babic@gmx.at>
2025-04-24 00:07:49 +0200

    van der pol convergence experiments

--- van_der_pol/run_convergence_experiments.py
+++ van_der_pol/run_convergence_experiments.py
@@ -5,20 +5,18 @@ from datetime import datetime
 from sklearn.metrics import mean_squared_error
 from configs.config_loader import load_config
 from model.neural_net import PhysicsInformedNN
-from model.plots import learning_curves, van_der_pol_dynamics, loss_over_tcoll, plot_regularization
 import pandas as pd
 import numpy as np
 import os
 import re
 import tensorflow as tf
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+import os
 
-NUM_SAMPLES_PER_FP = 200
 
-def grid_parameters(parameters: Dict[str, Iterable[Any]]) -> Iterable[Dict[str, Any]]:
-        for params in product(*parameters.values()):
-            yield dict(zip(parameters.keys(), params))
-            
-
+NUM_SAMPLES = 200
 
 config_base = load_config('configs/default.yaml')
 
@@ -34,140 +32,85 @@ config["T"] = 12.5
 config["freq_save"] = 0
 
 
-def run_without_reg():
-    results_list = []
-    config["regularization"] = "no_reg"
+x0s = np.random.normal(0, 0.5, size=NUM_SAMPLES)
+x_t0s = np.random.normal(0, 0.5, size=NUM_SAMPLES)
+
+
+dirname_no_reg = f"logs/models/visualization_experiments/no_reg"
+if not os.path.exists(dirname_no_reg):
+    os.makedirs(dirname_no_reg)
+
+# dirname_reg_derivative = f"logs/models/visualization_experiments/reg_derivative"
+# if not os.path.exists(dirname_reg_derivative):
+#     os.makedirs(dirname_reg_derivative)
+
+dirname_reg_derivative_unstable_fp = f"logs/models/visualization_experiments/reg_derivative_unstable_fp"
+if not os.path.exists(dirname_reg_derivative_unstable_fp):
+    os.makedirs(dirname_reg_derivative_unstable_fp)
+
+results_list = []
+
+for i in range(NUM_SAMPLES):
+    config["x0"] = float(x0s[i])
+    config["x_t0"] = float(x_t0s[i])
     
-    dirname = f"convergence_plots/no_reg"
-    if not os.path.exists(dirname):
-        os.makedirs(dirname)
+    try:
+        # Without regularization
+        config["regularization"] = "no_reg"
+        PINN = PhysicsInformedNN(config, verbose=True)
+        training_log = PINN.train()
 
-    losses = []
-    loss_successes = []
-        
-    table_list = []
+        PINN.save_weights(f"{dirname_no_reg}/run_{i}.pkl")
 
-    for fp in fixed_points:
-        x0s = np.random.normal(fp[0], 1.0, size=NUM_SAMPLES_PER_FP)
-        x_t0s = np.random.normal(fp[1], 1.0, size=NUM_SAMPLES_PER_FP)
-        print(x0s)
-        print(x_t0s)
-        for i in range(NUM_SAMPLES_PER_FP):
-            config["x0"] = float(x0s[i])
-            config["x_t0"] = float(x_t0s[i])
-                    
-            PINN = PhysicsInformedNN(config, verbose=True)
-            try:
-                training_log = PINN.train()
-            
-                # get reference solution (analytical)
-                t_line, x_true, x_t_true = PINN.data.reference()
+        t_line, x_true, x_t_true = PINN.data.reference()
 
-                xxt_true = np.concatenate([x_true.numpy(), x_t_true.numpy()], axis=1)
-                # get PINN prediction
-                with tf.GradientTape() as tape:
-                    tape.watch(t_line)
-                    x_pred = PINN(t_line)
-                    x_t_pred = tape.gradient(x_pred, t_line)
+        # get PINN prediction
+        x_pred = PINN(t_line)
+        loss = mean_squared_error(x_true.numpy(), x_pred.numpy())
+        loss_success_no_reg = (np.linalg.norm(x_true - x_pred) / np.linalg.norm(x_true)) < 0.15
 
-                loss = mean_squared_error(x_true.numpy(), x_pred.numpy()) 
-                loss_success = np.linalg.norm(x_true - x_pred) / np.linalg.norm(x_true) < 0.15
-        
-                x_last_pred = tf.reduce_mean(x_pred[-5:, 0])
-                x_t_last_pred = tf.reduce_mean(x_t_pred[-5:])
-                
-                closest_fp = None
-                stability = None
 
-                for fp, counter in fixed_points.items():
-                    if tf.linalg.norm(x_last_pred - fp[0]) < 0.1 and tf.linalg.norm(x_t_last_pred - fp[1]) < 0.1:
-                        closest_fp = fp
-                        stability = fixed_points[fp]
-                
-                table_entry = pd.DataFrame({"(x0, x_t0)": [(config["x0"], config["x_t0"])], 
-                                            "converged to": [(float(x_last_pred), float(x_t_last_pred))],
-                                            "closest fp": [closest_fp],
-                                            "stability": [stability],
-                                            "success": [loss_success]})
-                
-                table_list.append(table_entry)
-
-                van_der_pol_dynamics(PINN, f"{dirname}/fp{str(fp)}_run{i}.png")
-
-            except IndexError as e:
-                print(e)
-
-            table = pd.concat(table_list)
-            table.to_csv("convergence_points.csv")
-            
-                    
-def repeat_with_reg(file):
-    table_no_reg = pd.read_csv(file)
-    table_no_reg["x0"] = table_no_reg["(x0, x_t0)"].apply(lambda row: eval(row)[0])
-    table_no_reg["x_t0"] = table_no_reg["(x0, x_t0)"].apply(lambda row: eval(row)[1])
-    config["regularization"] = "reg_derivative_unstable_fp"
-
-    table_reg = table_no_reg
-    table_reg["converged to with reg"] = ""
-    table_reg["closest fp with reg"] = ""
-    table_reg["stability with reg"] = ""
-    table_reg["success reg"] = ""
-
-    dirname = f"convergence_plots/no_reg"
-
-    if not os.path.exists(dirname):
-        os.makedirs(dirname)
-
-    for i in range(table_no_reg.shape[0]):
-        config["x0"] = table_reg["x0"].iloc[i]
-        config["x_t0"] = table_reg["x_t0"].iloc[i]
+        """
+        # With time derivative regularization
+        config["regularizer"] = "reg_derivative"
         config["reg_coeff"] = 1.0
         config["reg_epochs"] = 0.5
         config["reg_decay"] = "linear"
-
-
         PINN = PhysicsInformedNN(config, verbose=True)
-        try:
-                training_log = PINN.train()
-            
-                # get reference solution (analytical)
-                t_line, x_true, x_t_true = PINN.data.reference()
+        training_log = PINN.train()
 
-                xxt_true = np.concatenate([x_true.numpy(), x_t_true.numpy()], axis=1)
-                # get PINN prediction
-                with tf.GradientTape() as tape:
-                    tape.watch(t_line)
-                    x_pred = PINN(t_line)
-                    x_t_pred = tape.gradient(x_pred, t_line)
+        PINN.save_weights(f"{dirname_reg_derivative}/run_{i}.pkl")
 
-                loss = mean_squared_error(x_true.numpy(), x_pred.numpy())
-                loss_success = np.linalg.norm(x_true - x_pred) / np.linalg.norm(x_true) < 0.15
+        # get PINN prediction
+        x_pred = PINN(t_line)
+        loss = mean_squared_error(x_true.numpy(), x_pred.numpy())
+        loss_success_reg_derivative = (np.linalg.norm(x_true - x_pred) / np.linalg.norm(x_true)) < 0.15
+        """
+
+        # With unstable fp regularization
+        config["regularization"] = "reg_derivative_unstable_fp"
+        config["reg_coeff"] = 1.0
+        config["reg_epochs"] = 0.5
+        config["reg_decay"] = "linear"
+        PINN = PhysicsInformedNN(config, verbose=True)
+        training_log = PINN.train()
         
-                x_last_pred = tf.reduce_mean(x_pred[-5:, 0])
-                x_t_last_pred = tf.reduce_mean(x_t_pred[-5:])
-                
-                closest_fp = None
-                stability = None
+        PINN.save_weights(f"{dirname_reg_derivative_unstable_fp}/run_{i}.pkl")
+        
+        # get PINN prediction
+        x_pred = PINN(t_line)
+        loss = mean_squared_error(x_true.numpy(), x_pred.numpy())
+        loss_success_reg_derivative_unstable_fp = (np.linalg.norm(x_true - x_pred) / np.linalg.norm(x_true)) < 0.15
 
-                for fp, counter in fixed_points.items():
-                    if tf.linalg.norm(x_last_pred - fp[0]) < 0.1 and tf.linalg.norm(x_t_last_pred - fp[1]) < 0.1:
-                        closest_fp = fp
-                        stability = fixed_points[fp]
-                
-                table_reg["converged to with reg"].iloc[i] = str(tuple((float(x_last_pred), float(x_t_last_pred))))
-                table_reg["closest fp with reg"].iloc[i] = str(closest_fp)
-                table_reg["stability with reg"].iloc[i] = str(stability)
-                table_reg["success reg"].iloc[i] = loss_success
+        table_entry = pd.DataFrame({"(x0, x_t0)": [(float(x0s[i]), float(x_t0s[i]))], 
+                                    "success_no_reg": [loss_success_no_reg],
+                                    "success_reg_derivative_unstable_fp": [loss_success_reg_derivative_unstable_fp]})
+        
+        results_list.append(table_entry)
+        results_table = pd.concat(results_list)
+        results_table.to_csv("visual_results_T_12_5.csv")
 
-                van_der_pol_dynamics(PINN, f"{dirname}/fp{str(fp)}_run{i}.png")
-                
-
-        except Exception as e:
-                print(e)
-
+    except Exception as e:
+        print(e)
             
-        table_reg.to_csv("convergence_points_reg.csv")
-
-
-run_without_reg()
-repeat_with_reg("convergence_points.csv")
+     
